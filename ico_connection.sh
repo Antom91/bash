0bb26c65d7ffcafdb6876beaee51240292e8e54a
Anton M <anton.marusenko@onix-systems.com>
2019-06-04 13:22:21 +0300

    Change connection vars

--- ico_connection.sh
+++ ico_connection.sh
@@ -2,8 +2,6 @@
 
 #Variables
 timeoutch=10 #Timeout to check connections
-SERVER_ADDR=127.0.0.1 # Adress for check
-SERVER_PORT=81 #Port for check
 dotpoint="." #Element to draw
 timeofico=0.2 #Time of icon symbols change
 
@@ -13,17 +11,29 @@ timend=$((SECONDS+$timeoutch)) #time for end check connection
 icons='\-/|'
 n=1 #number of position icon
 
+
+	if [ -z $SERVER_ADDR ]
+		then
+		SERVER_ADDR=127.0.0.1 #Adress for check
+	fi
+
+	if [ -z $SERVER_PORT ]
+		then
+		SERVER_PORT=80 #Port for check
+	fi
+
+
 #Function of check port
 checkp ()
 {
-nc -zv $SERVER_ADDR $SERVER_PORT -w 1 &> /dev/null bach; sta=$?
-sleep $timeofico
+	nc -zv $SERVER_ADDR $SERVER_PORT -w 1 &> /dev/null bach; sta=$?
+	sleep $timeofico
 }
 
 #function of loading status icon
 loadi ()
 {
-printf "\b${icons:n++%${#icons}:1}"
+	printf "\b${icons:n++%${#icons}:1}"
 }
 
 echo -e "Hello $USER, now you need to wait $timeoutch sec\n"

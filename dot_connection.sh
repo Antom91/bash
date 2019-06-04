dd34c29d2d04cca0734146ca7f5a35880ce6a10a
Anton M <anton.marusenko@onix-systems.com>
2019-06-04 14:37:57 +0300

    Rewrite dot_connection file

--- dot_connection.sh
+++ dot_connection.sh
@@ -2,32 +2,44 @@
 
 #Variables
 timeoutch=10 #Timeout to check connections
-SERVER_ADDR=127.0.0.1 # Adress for check
-SERVER_PORT=80 #Port for check
 dotpoint="." #Element to draw
 timend=$((SECONDS+$timeoutch)) #time for end check connection
+
+
+	if [ -z $SERVER_ADDR ]
+                then
+                SERVER_ADDR=127.0.0.1 #Adress for check
+        fi
+
+        if [ -z $SERVER_PORT ]
+                then
+                SERVER_PORT=80 #Port for check
+        fi
+
+#Function of check port
+checkp ()
+{
+	nc -zv $SERVER_ADDR $SERVER_PORT -w 1 &> /dev/null bach; sta=$? 
+	sleep 1
+}
 
 echo -e "Hello $USER, now you need to wait $timeoutch sec\n"
 echo -e "Waiting for connecting to server\c"
 
-	while [ $SECONDS -lt $timend ]
-		do
-		echo -n "$dotpoint"
+        while [ $SECONDS -lt $timend ]
+                do
+                checkp #Call function of  check port
+                if [[ $sta == 0 ]]
+                        then
+                        echo -e "\nThe port is active!\n"
+                        exit 0
+                else
+                echo -n "$dotpoint" #Call function of draw dot loading status 
+                fi
 
-	result=$(nc -zv $SERVER_ADDR $SERVER_PORT -w 1 2>&1 | grep -oh succeeded)
-
-
-		if [[ $result == "succeeded" ]]
-			then
-			echo -e "\nThe port is active!\n"
-			exit 0
-		else
-			sleep 1
-		fi
-
-	done
-
-               ! true #If not connection
-                echo -e "\nThe Port is Not Active. Error Code = $? \n"
-                exit 1
-
+        done
+                if [ $sta -ne 0 ]
+                        then
+                        echo -e "\nThe Port is Not Active. Error Code = $sta \n"
+                        exit 1
+                fi

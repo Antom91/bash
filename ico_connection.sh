913f233346616e6d02860b086169730b289acd86
Anton M <anton.marusenko@onix-systems.com>
2019-06-04 12:31:41 +0300

    Some Fixes

--- ico_connection.sh
+++ ico_connection.sh
@@ -5,27 +5,25 @@ timeoutch=10 #Timeout to check connections
 SERVER_ADDR=127.0.0.1 # Adress for check
 SERVER_PORT=80 #Port for check
 dotpoint="." #Element to draw
-timeofico=0.3 #Time of icon symbols change
+timeofico=0.2 #Time of icon symbols change
 
 timend=$((SECONDS+$timeoutch)) #time for end check connection
 
 #Variables to make loading status icon
-lo1='|'
-lo2='/'
-lo3='-'
-lo4='\'
+icons='\-/|'
+n=1 #number of position icon
+
+#Function of check port
+checkp ()
+{
+nc -zv $SERVER_ADDR $SERVER_PORT -w 1 &> /dev/null bach; sta=$?
+sleep $timeofico
+}
 
 #function of loading status icon
 loadi ()
 {
-        printf "\b$lo1"
-	sleep $timeofico
-        printf "\b$lo2"
-        sleep $timeofico
-	printf "\b$lo3"
-	sleep $timeofico
-        printf "\b$lo4"
-	sleep $timeofico
+printf "\b${icons:n++%${#icons}:1}"
 }
 
 echo -e "Hello $USER, now you need to wait $timeoutch sec\n"
@@ -33,17 +31,13 @@ echo -e "Waiting for connecting to server \c"
 
 	while [ $SECONDS -lt $timend ]
 		do
-		#echo -n "$dotpoint"
-
-		result=$(nc -zv $SERVER_ADDR $SERVER_PORT -w 1 2>&1 | grep -oh succeeded)
-
-
-		if [[ $result == "succeeded" ]]
+		checkp #Call function of  check port
+		if [[ $sta == 0 ]]
 			then
 			echo -e "\nThe port is active!\n"
 			exit 0
 		else
-		loadi
+		loadi #Call function of draw load icon status 
 		fi
 
 	done
